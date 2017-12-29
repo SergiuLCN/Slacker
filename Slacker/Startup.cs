5e529d5926e0f7e93386731a8a2a219a7b6d82c1
SergiuLCN <lucanu.sergiu@gmail.com>
2017-12-29 19:21:38 +0200

    Add project files.

---
+++ Slacker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Slacker.Startup))]
+namespace Slacker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
